8f66421f2e916f4191780cdac1e26af613107bb5
khalsa-ji <31163953+khalsa-ji@users.noreply.github.com>
2017-10-09 23:14:26 +0530

    C program for adding any two numbers.
    Added the functionality for adding any two real numbers.

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main()
+{
+	float a, b;
+	printf("Program to find the sum of any two given numbers\n\n");
+	printf("Enter the first number: ");
+	scanf("%f", &a);
+	printf("Enter the second number: ");
+	scanf("%f", &b);
+	printf("\nSum of %.2f and %.2f is %.2f.", a, b, a + b);
+  return 0;
+}

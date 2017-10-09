a506345bd83b5fd3a2ab3e2be23000396b359b3e
vlasebian <vlad_vitan@hotmail.com>
2017-10-09 23:10:25 +0300

    Adding fibonacci implementation

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,26 @@
 /* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(void)
+{
+    int n = 0, i = 0;
+
+    printf("Enter the number of terms:\n");
+    scanf("%d", &n);
+    for(i = 0; i < n; i ++)
+        printf("%d ", fibonacci(i));
+    printf("\n");
+    return 0;
+}

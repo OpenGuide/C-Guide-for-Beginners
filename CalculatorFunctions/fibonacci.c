311fe69784f0c37d891f3e64e9bee5cc53d82320
ben simms <ben@bensimms.moe>
2017-10-09 23:02:47 +0100

    Add: Iterative and recursive fibonacci

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,22 @@
-/* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+#include <assert.h>
+
+
+/* recursive fibonacci */
+int fib_rec(int n) {
+  assert(n >= 0);
+  if (n <= 1)
+    return 1;
+  return fib_rec(n - 1) + fib_rec(n - 2);
+}
+
+/* iterative fibonacci */
+int fib_iter(int n) {
+  assert(n >= 0);
+  int a = 0, b = 1;
+  int t;
+  while (n--) {
+    t = a + b;
+    a = b, b = t;
+  }
+  return b;
+}

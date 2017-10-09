a4d622a4665863b06050efeb58f2deb116864094
Abhishek Prasad <abhi_hk95@Abhisheks-MacBook-Air.local>
2017-10-10 03:45:56 +0530

    Fibonacci recursion

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,17 @@
 /* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+//Fibonacci Series using Recursion
+#include<stdio.h>
+int fib(int n)
+{
+   if (n <= 1)
+      return n;
+   return fib(n-1) + fib(n-2);
+}
+ 
+int main ()
+{
+  int n;
+  scanf("%d",&n);
+  printf("%d", fib(n));
+  return 1;
+}

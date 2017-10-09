19e76bc95e725b88fb49eda29ed16314af6d53cf
Júlio Fortunato <juliofortunato58@gmail.com>
2017-10-09 14:40:59 -0300

    Include add.c

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+
+    float firstNumber, secondNumber, sum;
+
+    printf("First number: ");
+    scanf("%f", &firstNumber);
+    printf("Second number: ");
+    scanf("%f", &secondNumber);
+
+    sum = firstNumber + secondNumber;
+
+    printf("\n%.2f + %.2f = %.2f\n", firstNumber, secondNumber, sum);
+    printf("The sum is: %.2f\n", sum);
+
+    return 0;
+}

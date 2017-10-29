63db58337a6188843c82d5f53ac32bd9b1a90658
Mangalam Gupta <b150073ee@nitsikkim.ac.in>
2017-10-30 01:46:15 +0530

    Update fibonacci.c

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,25 @@
 /* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+#include<stdio.h>
+ 
+int Fibonacci(int);
+int main()
+{
+   int n, i = 0, c;
+   scanf("%d",&n);
+   printf("Fibonacci series\n");
+   for ( c = 1 ; c <= n ; c++ )
+   {
+      printf("%d\n", Fibonacci(i));
+      i++; 
+   }
+   return 0;
+}
+int Fibonacci(int n)
+{
+   if ( n == 0 )
+      return 0;
+   else if ( n == 1 )
+      return 1;
+   else
+      return ( Fibonacci(n-1) + Fibonacci(n-2) );
+} 

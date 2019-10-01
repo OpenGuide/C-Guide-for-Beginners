c5ae1ea221cc2bc0b10d22335f6b4542d6d713db
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-10-01 17:50:22 -0300

    creating fibonacci sequence recursively
    this fibonacci C file is an sequence 'generator' using recursively definition

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,23 @@
-/* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibo_r(int n){
+	if (n == 1 || n == 2){
+	//	printf("1\n");
+		return 1;
+	}else{
+//		printf("%d", (fibo_r(n-1) + fibo_r(n-2)));
+		return (fibo_r(n-1) + fibo_r(n-2));
+	}
+}
+
+int main(){
+	int number;
+	printf("Typen any number: ");
+	scanf("%d", &number);
+	for(int i = 0; i < number; i++){
+        int fib = fibo_r(i+1);
+    	printf("%d\n", fib);
+//		printf("%d ", fibo_r(i+1));
+	}
+}

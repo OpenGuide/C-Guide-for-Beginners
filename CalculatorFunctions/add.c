ee82142c3e825a8de4049ac35f57153e562592ff
Raunakk02 <56073198+Raunakk02@users.noreply.github.com>
2019-10-03 16:33:53 +0530

    added add.c code by Raunakk02

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,14 @@
+/**************************************************************************
+DATED: 03/10/2019                                      AUTHOR: RAUNAK KUMAR
 
+                       PROGRAM TO ADD TWO NUMBERS
+**************************************************************************/
+
+int main()
+{
+   int a,b;
+  printf("ENTER TWO NUMBERS: ");
+  scanf("%d%d",&a,&b);
+  printf("THE SUM OF %d AND %d IS : %d.",a,b,a+b);
+  return 0;
+}

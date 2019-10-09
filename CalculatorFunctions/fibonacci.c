b8e24f161a3914a717760af7677e9f4476cdd72e
cham2601 <chamanthyarlagadda@gmail.com>
2019-10-10 00:39:11 +0530

    added add.c,fibonacci.c,Linear_Search.c

--- CalculatorFunctions/fibonacci.c
+++ CalculatorFunctions/fibonacci.c
@@ -1,1 +1,19 @@
-/* Make a function that calculates the first n fibonacci numbers. Bonus points if it is recursive! */
+/*  Prints the first n values of fibonacci series  */
+
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter size of fibonacci series\n");
+    scanf("%d",&n);
+    printf("Fibonacci series of size %d is\n",n);
+    printf("%d %d ",0,1);
+    int a=0,b=1;
+    for(int i=2;i<n;i++){
+        int c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    printf("\n");
+}
--- SearchAlgorithms/Linear_Search.c
+++ SearchAlgorithms/Linear_Search.c
@@ -1,1 +1,30 @@
+/* This program finds the existence of an element in a given array*/
 
+
+#include<stdio.h>
+int main(){
+    int n,m,flg=0;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the values of array of size %d\n",n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int i=0;
+    printf("Enter the value that should be searched\n");
+    scanf("%d",&m);
+
+/*  While loop continues to run untill 
+    flg becomes 1 when value is found
+                  or
+    array is fully checked and value not found    */
+    while(flg==0 && i<n){
+        if(m==arr[i]){
+            flg=1;
+            printf("%d is found at position %d in given array\n",m,i);
+        }
+        i++;
+    }
+    if(flg==0) printf("%d is not found in given array\n",m);
+}

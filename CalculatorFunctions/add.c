035eae0e59bdb725dc69391ba701f45881fa95d0
VitorLamego <vitorlamegoo@gmail.com>
2019-10-15 16:43:10 -0300

    Criação do código em C para soma de dois números

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main (){
+    //SEÇÃO DE DECLARAÇÃO
+    float a,b;
+    float soma;
 
+    //SEÇÃO DE COMANDOS 
+    scanf ("%f %f",&a,&b);
+    soma = a+b;
+    printf ("%.2f\n",soma);
+
+    return 0;
+}

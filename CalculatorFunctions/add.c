bea0b81700ea1e349d428733c8458daac1da836b
Miltos Katifedenios <miltoskat@hotmail.com>
2019-10-17 15:12:50 +0300

    Update add.c
    Added code in add.c

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,26 @@
+/**
+ * C program to find sum of two numbers
+ */
 
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+
+    /*
+     * Read two numbers from user
+     */
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number:");
+    scanf("%d", &num2);
+
+    /* Adding both number is simple and fundamental */
+    sum = num1 + num2;
+
+    /* Prints the sum of two numbers */
+    printf("Sum of %d and %d = %d", num1, num2, sum);
+
+    return 0;
+}

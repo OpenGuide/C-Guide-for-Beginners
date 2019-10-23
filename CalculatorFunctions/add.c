f06ae2da8454ac1a8da011d1c79a8cbe073f179d
Hrishi Bodkhe <hrishikesh.b9315@gmail.com>
2019-10-23 08:15:51 +0530

    Created the add.c file

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+double add(double a, double b)
+{
+    double c = a + b;
+    return c;
+}
+
+int main()
+{
+    double a, b;
+    printf("Enter 1st no.: ");
+    scanf("%lf", &a);
+    printf("Enter 1st no.: ");
+    scanf("%lf", &b);
+    double result = add(a, b);
+    printf("Result: %lf", result);
+    return 0;
+}

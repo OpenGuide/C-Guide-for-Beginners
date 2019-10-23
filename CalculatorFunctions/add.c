5c9f45f9948e84d886f414df46fa87aade3f26df
TSaiCharan <sai60095@gmail.com>
2019-10-23 19:18:59 +0530

    adds to Searching and Calculator

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+#define Add(a,b) a+b
+
+int main() 
+
+{
+    
+	int a,b;
+    
+	printf("Enter two numbera to make addition:");
+    
+	scanf("%d%d",&a,&b);
+    
+	printf("%d+%d=%d",a,b,Add(a,b));
+    
+	return 0;
+
+}
--- SearchAlgorithms/Linear_Search.c
+++ SearchAlgorithms/Linear_Search.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+void linear_search(int n,int a[],int num)
+{
+	if(n>=0)
+		if(a[n]==num)
+			printf("%d element is found at %d position",num,n+1);
+		else
+			linear_search(n-1,a,num);
+	else
+		printf("%d element is not found",num);
+}
+int main()
+{
+	int n;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		printf("\nEnter element number %d:",i+1);
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter element to be searched:");
+	int num;
+	scanf("%d",&num);
+	linear_search(n-1,arr,num);
+	return 0;
+}

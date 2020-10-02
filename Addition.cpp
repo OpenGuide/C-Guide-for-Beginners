2ae31f04f300e129f0f55cba00787c6b54370116
imnrb <67147637+imnrb@users.noreply.github.com>
2020-10-02 12:48:11 +0530

    Create Addition.cpp
    Program to add to numbers

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+//ADDITION OF TWO NUMBERS
+
+
+#include<stdio.h> 
+int main()      
+{
+ int first,second,add; //declaring variables
+ printf("Enter two numbers"); 
+ scanf("%d %d",&first,&second);  //taking two input
+ add=first+second;   //adding first and second
+ printf(" Addition of two numbers %d",add);
+ return 0;
+}

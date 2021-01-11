e67d61c7b9f22281e21fa020e14c1b4c74ed69d2
Princy <princybodhiyan@gmail.com>
2021-01-11 10:49:00 +0530

    issue solved

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>          //header file
+int main()         //main function
+{
+    int a,b,sum;              //declaration of variables
+    printf("enter numbers to be added\n");
+    printf("enter first number: ");
+    scanf("%d",&a);        
+    printf("enter second number: ");
+    scanf("%d",&b);
+    sum=a+b;            //to perform addition
+    printf("sum of numbers is %d",sum); //to print result
+    }

fb41d4efb9e504010ee325d5fd2bbd1021f89ed3
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-01-18 12:45:58 +0530

    Update add.c

--- CalculatorFunctions/add.c
+++ CalculatorFunctions/add.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int Add(int i, int j);
+int main(){
+	int i,j;
+	printf("Enter two number: \n");
+	scanf("%d %d", &i,&j);
+	int r = Add(i,j);
+	printf("%d",r);
+	return 0;
+}
+ int Add(int i, int j)
+ {
+ 	int add = i + j;
+ 	return add;
+ }

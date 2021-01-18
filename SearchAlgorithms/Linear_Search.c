7f8e9d8ae6f81d48a08806ed4167132e04e9425d
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-01-18 13:08:49 +0530

    Update Linear_Search.c

--- SearchAlgorithms/Linear_Search.c
+++ SearchAlgorithms/Linear_Search.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int search(int arr[], int n, int x);
+int main(void) 
+{ 
+    int arr[100], x, n;
+    printf("Enter the no of elements: "); scanf("%d",&n);
+    printf("Enter all the elements\n");
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    printf("Enter the element you want to check: ");
+    scanf("%d\n",&x);
+    int result = search(arr, n, x);
+    if(result == -1)  printf("Element is not present in array");
+    else printf("Element is present at index %d", result);
+    return 0; 
+}
+int search(int arr[], int n, int x) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        if (arr[i] == x) 
+            return i; 
+    return -1; 
+} 
